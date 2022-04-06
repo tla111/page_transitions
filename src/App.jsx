55adfbb0e36722c0b74c549b2e1da2975feead5b
Tim La <tlamarketing111@gmail.com>
2022-04-06 12:17:42 -0400

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { Navbar, Homepage, CardsPage } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Page Transitions</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cards" element={<CardsPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
